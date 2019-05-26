f203f0b1a21278714b8c9a750ad1738388c83a80
jaeju.jang <jjj0611@gmail.com>
2019-05-26 16:45:37 +0900

    [ADD] 사용자에게 숫자를 입력 받아 각 수자의 자리 값을 더하는 예제를 작성하였습니다.

---
+++ 20190222/src/com/j_graphy/day02/FlowEx25.java
@@ -0,0 +1,30 @@
+package com.j_graphy.day02;
+
+import java.util.Scanner;
+
+public class FlowEx25 {
+
+	public static void main(String[] args) {
+		
+		int num = 0;
+		int sum = 0;
+		
+		System.out.print("숫자를 입력하세요. (예: 12345) >");
+		Scanner scanner = new Scanner(System.in);
+		String tmp = scanner.nextLine();
+		num = Integer.parseInt(tmp);
+//		while(num != 0) {
+//			sum += num % 10;
+//			System.out.printf("sum = %3d num = %d%n", sum, num);
+//			num /= 10;
+//		}
+		
+		for (;num !=0;) {
+			sum += num % 10;
+			System.out.printf("sum = %3d num = %d%n", sum, num);
+			num /= 10;
+		}
+		System.out.println("각 자리 수의 합 : " + sum);
+		
+	}
+}
