24cd9ab1955d9b058796289aafe1e14a42014c5b
jaeju.jang <jjj0611@gmail.com>
2019-06-06 21:00:38 +0900

    [ADD] Vector클래스 학습을 위해 Object 배열을 내부적으로 가지고 있는 클래스를 생성하였습니다.

---
+++ 20190306/src/com/j_graphy/day09/VectorEx2_01.java
@@ -0,0 +1,15 @@
+package com.j_graphy.day09;
+
+import java.util.*;
+
+//Q. 2-1 Object 매열 objArr를 인스턴스 변수로 갖는 MyVector 클래스를 선언하라
+
+//class MyVector {
+//	
+//	Object[] objArr;
+//	
+//}
+
+public class VectorEx2_01 {
+
+}
