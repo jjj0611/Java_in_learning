077ef083e9ab18403c795d3c573e72a879f4be73
jaeju.jang <jjj0611@gmail.com>
2019-06-06 21:17:00 +0900

    [ADD] ArrayList로 Iterator 객체를 만들어 사용하는 예제를 작성하였습니다.

---
+++ 20190307/src/com/j_graphy/day10/IteratorEx1.java
@@ -0,0 +1,26 @@
+package com.j_graphy.day10;
+
+import java.util.*;
+
+public class IteratorEx1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList list = new ArrayList();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		
+		Iterator it = list.iterator();
+		
+		Object obj;
+		while(it.hasNext()) {
+			obj = it.next();
+			System.out.println(obj);
+		}
+		
+	}
+	
+}
